f2defd3d4282ce76697c0ad27d897d6ed63dd5cc
Arpit Shah <arpit@Trinas-MacBook-Pro.local>
2017-06-13 14:59:30 -0700

    ES6 new syntax

---
+++ app/js/logger/lexical_this.js
@@ -0,0 +1,37 @@
+//ES5
+const oldperson = {
+  name: 'John',
+  cars: ['Ferrari','BMW'],
+  getInfo: function(){
+    this.cars.forEach(function(car){
+        log(`${this.name} has ${car}`)
+    });
+  }
+}
+// Error: TypeError: can`t read property 'name' of undefined
+//        as this no longer refers to person object
+
+//ES 5 Fix
+const person = {
+  name: 'John',
+  cars: ['Ferrari','BMW'],
+  getInfo: function(){
+    this.cars.forEach(function(car){
+        log(`${this.name} has ${car}`)
+    }.bind(this));
+  }
+}
+person.getInfo()
+// Bind function to person 'this' or add var that = this to fix the issue
+
+//ES6 using arrow function
+const newPerson = {
+  name: 'Bob',
+  cars: ['Ferrari','BMW'],
+  getInfo(){
+    this.cars.forEach(car => {
+        log(`${this.name} has ${car}`)
+    });
+  }
+}
+newPerson.getInfo();
---
+++ app/js/logger/spread_operator.js
@@ -0,0 +1,34 @@
+//concat array
+const arr1 = ['John','Bob','Chris']
+const arr2 = ['Don','Eyrie','Frank']
+const members = [...arr1, ...arr2]
+//log(members)
+
+
+//string to array
+const name = "John";
+const nametoArray = [...name];
+//log(nametoArray);
+
+//functions
+const addNumbers = function(n1,n2,n3){
+  return n1+n2+n3
+}
+const numbers = [1,3,5]
+//const result = addNumbers(numbers[0],numbers[1],numbers[2])
+//const result = addNumbers.apply(null,numbers);
+const result = addNumbers(...numbers)
+//log(result);
+
+//Objects
+
+const pname = {
+  firstname: "John",
+  lastname: 'Doe'
+}
+const address = {
+  city: 'SF',
+  country: 'USA'
+}
+const person = {...address, ...pname}
+//log(JSON.stringify(person,null,2))
